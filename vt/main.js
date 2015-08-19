8ae4e345d01d2333630038766988330e908be426
Gabriel Correia <gabriel.mfcorreia@gmail.com>
2015-08-19 15:09:54 -0300

    Create main.js

---
+++ vt/main.js
@@ -0,0 +1,38 @@
+var imageAddr = "http://www.kenrockwell.com/contax/images/g2/examples/31120037-5mb.jpg"; 
+var downloadSize = 4995374; //bytes
+
+window.onload = function() {
+    var oProgress = document.getElementById("progress");
+    oProgress.innerHTML = "Carregando imagem, isso pode demorar um pouco...";
+    window.setTimeout(MeasureConnectionSpeed, 1);
+};
+
+function MeasureConnectionSpeed() {
+    var oProgress = document.getElementById("progress");
+    var startTime, endTime;
+    var download = new Image();
+    download.onload = function () {
+        endTime = (new Date()).getTime();
+        showResults();
+    }
+
+    download.onerror = function (err, msg) {
+        oProgress.innerHTML = "ERRO!, imagem invalida, ou 404";
+    }
+
+    startTime = (new Date()).getTime();
+    var cacheBuster = "?nnn=" + startTime;
+    download.src = imageAddr + cacheBuster;
+
+    function showResults() {
+        var duration = (endTime - startTime) / 1000;
+        var bitsLoaded = downloadSize * 8;
+        var speedBps = (bitsLoaded / duration).toFixed(2);
+        var speedKbps = (speedBps / 1024).toFixed(2);
+        var speedMbps = (speedKbps / 1024).toFixed(2);
+        oProgress.innerHTML = "Sua velocidade: <br />" + 
+           speedBps + " bps<br />"   + 
+           speedKbps + " kbps<br />" + 
+           speedMbps + " Mbps<br />";
+    }
+}
