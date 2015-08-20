9471150c353a2ae0012e2ceca804265e9e56e6c8
Gabriel Correia <gabriel.mfcorreia@gmail.com>
2015-08-20 08:36:28 -0300

    Update main.js

--- vt/main.js
+++ vt/main.js
@@ -32,7 +32,7 @@ function MeasureConnectionSpeed() {
     download.src = imageAddr + cacheBuster;
 
     function showResults() {
-        var duration = (endTime - startTime) / 1000;
+        var duration = ((endTime - startTime) - 10) / 1000;
         var bitsLoaded = downloadSize * 8;
         var speedBps = (bitsLoaded / duration).toFixed(2);
         var speedKbps = (speedBps / 1024).toFixed(2);
