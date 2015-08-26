f508446575ced9bdb4243e5ecd6023e47a1b57c7
Gabriel Correia <gabriel.mfcorreia@gmail.com>
2015-08-26 15:14:08 -0300

    Update howtos.js

--- growndUpVT/js/howtos.js
+++ growndUpVT/js/howtos.js
@@ -1,5 +1,5 @@
 var refr = true;
-var difCounter = 0;
+var difCounter = 17;
 var g1;
 var g1 = new JustGage({
 	id: "g1",
@@ -24,6 +24,17 @@ var g2 = new JustGage({
 	relativeGaugeSize: true,
 	donut: true
 });
+var g3;
+var g3 = new JustGage({
+	id: "g3",
+	value: 100,
+	min: 0,
+	max: 100,
+	title: "Erro",
+	label: "%",
+	formatNumber: true,
+	relativeGaugeSize: true
+});
 
 function customValue(val) {
 	if (val < 50) {
@@ -38,9 +49,10 @@ function customValue(val) {
 
 $('#results').on("me", function () {
 	if(refr){
-		difCounter +=5;
-		g1.refresh(difCounter*1024);
-		g2.refresh(difCounter*2.8);
+		difCounter -=5;
+		//g1.refresh(difCounter*1024);
+		//g2.refresh(difCounter*2.8);
+		g3.refresh(difCounter*5.8);
 	}
 });
 
@@ -66,6 +78,7 @@ BOOMR.subscribe('before_beacon', function(o) {
 	if(o.t_done) { html += "<span>A pagina demorou " + o.t_done + " ms para carregar</span>"; }
 	if(o.bw) { html += "<span>Sua conexão é de " + parseInt((o.bw*8/1024)/1024) + "Mbps (Erro de &#x00b1;" + parseInt(o.bw_err*100/o.bw) + "%)</span>"; 
 		g1.refresh((o.bw*8/1024));
+		g3.refresh(o.bw_err*100/o.bw);
 		refr = false;
 	}
 	if(o.lat) { html += "<span>Seu ping: " + parseInt(o.lat) + "ms (Erro de &#x00b1;" + o.lat_err + "ms)</span>"; 
